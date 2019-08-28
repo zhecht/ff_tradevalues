74224492f6718817bc5da2261327da103379f9fc
Zachary Hecht <zhecht@eng-zhecht-mb.attlocal.net>
2019-08-27 21:28:43 -0400

    First commit. All the source code for version 1.0 of the extension

---
+++ content_scripts/main.js
@@ -0,0 +1,241 @@
+
+String.prototype.format = function () {
+	var i = 0, args = arguments;
+	return this.replace(/{}/g, function () {
+		return typeof args[i] != 'undefined' ? args[i++] : '';
+	});
+};
+
+function formatArgs(trade_results) {
+	var args = "?";
+	args += "team_0_len="+trade_results[0]["players"].length;
+	args += "&team_1_len="+trade_results[1]["players"].length;
+	args += "&evaluate="+evaluate;
+	args += "&is_espn="+is_espn;
+	args += "&is_nfl="+is_nfl;
+
+	for (var i = 0; i < 2; ++i) {
+		var players = trade_results[i]["players"];
+		for (var p = 0; p < players.length; ++p) {
+			args += "&team_"+i+"_player_"+p+"="+players[p];
+		}
+	}
+	return encodeURI(args);
+}
+
+function getOffenseIndexes(tables) {
+	var indexes = [];
+	for (var i = 0; i < tables.length; ++i) {
+		try {
+			var ths = tables[i].getElementsByTagName("thead")[0].getElementsByTagName("tr")[1].getElementsByTagName("th");
+			//var which = ths[1].getElementsByTagName("div")[0].innerHTML;
+			if (ths[0].getElementsByTagName("div")[0].innerHTML === "Offense" || ths[1].getElementsByTagName("div")[0].innerHTML === "Offense") {
+				indexes.push(i);
+			}
+		} catch (err) {
+
+		}
+	}
+	return indexes;
+}
+
+var is_espn = (window.location.host.indexOf("espn") != -1);
+var is_nfl = window.location.host == "fantasy.nfl.com";
+var is_yahoo = window.location.host == "football.fantasysports.yahoo.com";
+var path = window.location.pathname.split("/").pop();
+var trade_results = [ {"players": []}, {"players": []} ];
+var players_picked = false, evaluate = false, viewtrade = false;
+
+if (!is_espn && !is_nfl && !is_yahoo) {
+	throw new Error("Not a fantasy site! Returning");
+}
+
+if (is_espn) {
+	var step2 = window.location.search.substring(1).indexOf("step=2") !== -1;
+	
+	if (step2) {
+		var tables = document.getElementsByClassName("propose-trade-content")[0].children[1].children;
+	} else {
+		var tables = document.getElementsByClassName("Table2__right-aligned");
+	}
+	var idx = 0;
+	for (var i = 0; i < tables.length; ++i) {
+		//console.log(tables[i].getElementsByTagName("span")[0].innerText, tables[i].getElementsByTagName("span")[0].innerText == "Your Players");
+		if (tables[i].getElementsByTagName("span")[0].innerText == "Your Players") {
+			idx = 1;
+		}
+
+		names = tables[i].getElementsByClassName("player-column_info");
+		
+		var inputs;
+		if (!step2 || (step2 && idx == 1)) {
+			var inputs = tables[i].getElementsByTagName("button");
+		}
+		//console.log(names, tables[i], inputs);
+		for (var j = 0; j < names.length; ++j) {
+			if (idx == 0 && step2) {
+				var checked = true;
+			} else {
+				var checked = false;
+				if ((" " + inputs[j].className + " ").indexOf(" isActive ") > -1 ) {
+					checked = true;
+				}
+			}
+			
+			var name = names[j].getElementsByTagName("a")[0].innerHTML;
+			var team = names[j].getElementsByClassName("playerinfo__playerteam")[0].innerText;
+			var pos = names[j].getElementsByClassName("playerinfo__playerpos")[0].innerText;
+
+			trade_results[idx]["players"].push("{},{},{},{}".format(name,team,pos,checked));
+		}
+	}
+} else if (is_nfl) {
+
+	if (path == "tradeproposereview") {
+		var tables = document.getElementsByClassName("tableWrap");
+		var team_idx = 0;
+		for (var i = 0; i < tables.length; ++i) {
+			if (tables[i].getElementsByTagName("h4").length > 0 && tables[i].getElementsByTagName("h4")[0].innerText == "Players to Drop") {
+				console.log("continuing");
+				continue;
+			}
+			if (tables[i].getElementsByTagName("h4").length > 0 && tables[i].getElementsByTagName("h4")[0].innerText.indexOf("Your Team") > -1) {
+				team_idx = 1;
+			}
+			var names = tables[i].getElementsByClassName("playerNameAndInfo");
+			for (var j = 0; j < names.length; ++j) {
+				if (names[j].nodeName == "TD") {
+					var full = names[j].getElementsByTagName("a")[0].innerHTML;
+					var pos_team = names[j].getElementsByTagName("em")[0].innerHTML;
+					var pos = pos_team.split(" - ")[0];
+					var team = pos_team.split(" - ")[1];
+					var checked = false;
+
+					trade_results[team_idx]["players"].push("{},{},{},{}".format(full,team,pos,checked));
+				}
+			}
+		}
+	} else {
+		var in_display = [];
+		var rows = document.getElementsByClassName("displayTables")[0].getElementsByTagName("tr");
+		for (var i = 0; i < rows.length; ++i) {
+			if (rows[i].className.indexOf("player-") > -1 && rows[i].className.indexOf("show") > -1) {
+				in_display.push(rows[i].getElementsByClassName("playerNameAndInfo")[0].getElementsByTagName("a")[0].innerHTML);
+			}
+		}
+		var tables = [document.getElementsByClassName("actionTables")[0], document.getElementsByClassName("otherRoster")[0]];
+		for (var i = 0; i < 2; ++i) {
+			var names = tables[i].getElementsByClassName("playerNameAndInfo");
+			var inputs = tables[i].getElementsByTagName("input");
+			var k = 0;
+			for (var j = 0; j < names.length; ++j) {
+				if (names[j].nodeName == "TD") {
+					var full = names[j].getElementsByTagName("a")[0].innerHTML;
+					var pos_team = names[j].getElementsByTagName("em")[0].innerHTML;
+					var pos = pos_team.split(" - ")[0];
+					var team = pos_team.split(" - ")[1];
+					var checked = false;
+					
+					if (in_display.indexOf(full) > -1) {
+						checked = true;
+					} else if (i == 0) {
+						checked = inputs[k].checked;
+					}
+					k++;
+					trade_results[i]["players"].push("{},{},{},{}".format(full,team,pos,checked));
+				}
+			}
+		}
+	}
+} else {
+	var viewtrade = (window.location.pathname.indexOf("viewtrade") !== -1);
+	var players_picked = (window.location.search.indexOf("stage=1") === -1) || viewtrade;
+	var tradeform = document.getElementById("proposetradeform");
+	var evaluate = (document.getElementsByTagName("h1")[0].innerHTML === "Evaluate Trade");
+
+	console.log(viewtrade, players_picked, evaluate);
+
+	var tables;
+	if (evaluate) {
+		tradeform = document.getElementById("evaluate-players");
+		tables = [tradeform.getElementsByTagName("section")[0], tradeform.getElementsByTagName("section")[1]]
+	} else {
+		var indexes = getOffenseIndexes(document.getElementsByClassName("Table"));
+		tables = [document.getElementsByClassName("Table")[indexes[0]], document.getElementsByClassName("Table")[indexes[1]]];
+	}
+
+	for (var i = 0; i < 2; ++i) {
+		var names = tables[i].getElementsByClassName("ysf-player-name");
+		if (evaluate) {
+			names = tables[i].getElementsByClassName("player-details");
+		}
+		
+		var inputs = tables[i].getElementsByTagName("input");
+		var empty_len = 0;
+		for (var j = 0; j < names.length; ++j) {
+			var checked = false;
+			if (names[j].getElementsByTagName("span")[0].innerHTML !== "(Empty)") {
+				var span = names[j].getElementsByClassName("Fz-xxs")[0].innerHTML;
+				if (evaluate) {
+					span = names[j].getElementsByClassName("Fz-xxs")[1].innerHTML;
+				}
+
+			
+				if (!players_picked) {
+					checked = inputs[j - empty_len].checked;
+				}
+				var name = names[j].getElementsByTagName("a")[0].innerHTML;
+				var team = span.split(" - ")[0];
+				var pos = span.split(" - ")[1];
+				trade_results[i]["players"].push("{},{},{},{}".format(name,team,pos,checked));
+			} else {
+				empty_len++;
+			}
+		}
+	}
+}
+
+var team_name0, team_name1;
+if (is_espn) {
+	team_name0 = document.getElementsByClassName("mh3")[0].innerText;
+	team_name1 = "Your Players";
+	evaluate = false;
+	players_picked = false;
+} else if (is_nfl) {
+	evaluate = false;
+	players_picked = false;
+	if (path == "tradeproposereview") {
+		team_name0 = document.getElementsByClassName("tableWrap")[0].getElementsByTagName("h4")[0].innerText.split("Trade For from ")[1];
+		players_picked = true;
+		team_name1 = "My Team";
+	} else if (path == "tradeproposeselecttraderplayers") {
+		team_name0 = "My Team";
+		team_name1 = document.getElementsByClassName("otherRoster")[0].getElementsByTagName("h4")[0].innerHTML.split(" Roster")[0];
+	} else {
+		team_name0 = document.getElementsByClassName("actionTables")[0].getElementsByTagName("h4")[0].innerHTML.split(" Player Roster")[0];
+		team_name1 = "My Team";
+	}
+} else {
+	if (evaluate) {
+		team_name0 = document.getElementById("evaluate-players").getElementsByTagName("h3")[0].getElementsByTagName("a")[0].innerHTML;
+		team_name1 = document.getElementById("evaluate-players").getElementsByTagName("h3")[1].getElementsByTagName("a")[0].innerHTML;
+	} else if (viewtrade) {
+		team_name0 = document.getElementById("viewtradeactions").getElementsByTagName("h2")[0].innerHTML;
+		team_name1 = document.getElementById("viewtradeactions").getElementsByTagName("h2")[1].innerHTML;
+	} else {
+		team_name0 = document.getElementById("proposetradeform").getElementsByTagName("h2")[0].innerHTML;
+		team_name1 = document.getElementById("proposetradeform").getElementsByTagName("h2")[1].innerHTML;
+	}
+}
+
+var args = formatArgs(trade_results);
+browser.storage.local.set({
+	args: args,
+	team_name0: team_name0,
+	team_name1: team_name1,
+	evaluate: evaluate,
+	viewtrade: viewtrade,
+	players_picked: players_picked,
+	is_espn: is_espn,
+	is_nfl:  is_nfl
+}, function() {});
---
+++ popup/popup.js
@@ -0,0 +1,229 @@
+
+
+String.prototype.format = function () {
+	var i = 0, args = arguments;
+	return this.replace(/{}/g, function () {
+		return typeof args[i] != 'undefined' ? args[i++] : '';
+	});
+};
+
+function getName(result) {
+	if (!result)
+		return "";
+	var split_data = result.split(",");
+	return split_data[0];
+}
+
+function getVal(result) {
+	if (!result)
+		return "";
+	var split_data = result.split(",");
+	return parseFloat(split_data[1]);
+}
+
+function fillTable(results) {
+	document.getElementsByClassName("name")[0].innerText = results["team_name0"];
+	document.getElementsByClassName("name")[1].innerText = results["team_name1"];
+
+	var player_len = (results["team0"].length >= results["team1"].length) ? results["team0"].length : results["team1"].length;
+
+	var tot0 = 0, tot1 = 0;
+	for (var p = 0; p < player_len; ++p) {
+		var name0 = "", name1 = "", val0 = "", val1 = "";
+		var display_val0 = "", display_val1 = "";
+		var class0 = "player_td", class1 = "player_td";
+		if (p < results["team0"].length) {
+			var sp = results["team0"][p].split(",");
+			name0 = sp[0];
+			val0 =  sp[1];
+			display_val0 = parseFloat(sp[1].split("_")[1]); //get half point
+
+			if (players_picked) {
+				tot0 += display_val0;
+			} else if (sp[2] === "true") {
+				tot0 += display_val0;
+				class0 += " clicked";	
+			}
+		}
+		if (p < results["team1"].length) {
+			var sp = results["team1"][p].split(",");
+			name1 = sp[0];
+			val1 = sp[1];
+			display_val1 = parseFloat(sp[1].split("_")[1]);
+
+			if (players_picked) {
+				tot1 += display_val1;
+			} else if (sp[2] === "true") {
+				tot1 += display_val1;
+				class1 += " clicked";	
+			}
+		}
+		var tr = document.createElement("tr"); tr.className = "player_row";
+		var td = document.createElement("td");
+		td.className = class0;
+		td.id = "0_{}".format(val0);
+		td.innerText = "{} - {}".format(display_val0, name0);
+
+		var td2 = document.createElement("td");
+		td2.className = class1;
+		td2.id = "1_{}".format(val1);
+		td2.innerText = "{} - {}".format(display_val1, name1);
+
+		tr.appendChild(td); tr.appendChild(td2);
+		document.getElementById("table").appendChild(tr);
+	}
+	// black bar
+	var tr = document.createElement("tr");
+	var td = document.createElement("td");
+	td.colSpan = "2";
+	td.style = "background-color: black; opacity: .4;";
+	tr.appendChild(td);
+	document.getElementById("table").appendChild(tr);
+
+	// total display
+	var tr = document.createElement("tr");
+	var td = document.createElement("td");
+	td.id = "total0";
+	td.innerText = tot0;
+
+	var td2 = document.createElement("td");
+	td2.id = "total1";
+	td2.innerText = tot1;
+	tr.appendChild(td);
+	tr.appendChild(td2);
+	document.getElementById("table").appendChild(tr);
+
+	findBest();
+}
+
+function findBest() {
+	var tot0 = parseFloat(document.getElementById("total0").innerText);
+	var tot1 = parseFloat(document.getElementById("total1").innerText);
+	var class0 = (tot0 >= tot1) ? "best" : "";
+	var class1 = (tot1 >= tot0) ? "best" : "";
+	document.getElementById("total0").className = class0;
+	document.getElementById("total1").className = class1;
+}
+
+function resetTotal() {
+	document.getElementById("total0").innerText = 0;
+	document.getElementById("total1").innerText = 0;
+}
+
+function resetBtns() {
+	var btns = document.getElementsByTagName("button");
+	for (var i = 0; i < btns.length; ++i) {
+		btns[i].className = "";
+	}
+}
+
+var changeScoring = function() {
+	var scoring_idx = parseInt(this.id);
+	document.getElementById("scoring_idx").value = scoring_idx;
+
+	resetBtns();
+	this.className = "active";
+
+	var rows = document.getElementsByClassName("player_row");
+	var tot0 = 0, tot1 = 0;
+	for (var i = 0; i < rows.length; ++i) {
+		var tds = rows[i].getElementsByTagName("td");
+		var sp0 = tds[0].id.split("_");
+		var sp1 = tds[1].id.split("_");
+		var val0 = 0, val1 = 0;
+
+		if (sp0.length > 2) {
+			var val0 = parseFloat(sp0[scoring_idx]);
+			tds[0].innerText = val0 + " - " + tds[0].innerText.split(" - ")[1];
+			if (players_picked) {
+				tot0 += val0;
+			} else {
+				if ((" " + tds[0].className + " ").indexOf(" clicked ") > -1 ) {
+					tot0 += val0;
+				}
+			}
+		}
+		if (sp1.length > 2) {
+			var val1 = parseFloat(sp1[scoring_idx]);
+			tds[1].innerText = val1 + " - " + tds[1].innerText.split(" - ")[1];
+			if (players_picked) {
+				tot1 += val1;
+			} else {
+				if ((" " + tds[1].className + " ").indexOf(" clicked ") > -1 ) {
+					tot1 += val1;
+				}
+			}
+		}
+	}
+	document.getElementById("total0").innerText = tot0;
+	document.getElementById("total1").innerText = tot1;
+	findBest();
+}
+
+var increment = function() {
+	var team = parseInt(this.id.split("_")[0]);
+	var scoring_idx = parseFloat(document.getElementById("scoring_idx").value);
+	var val = parseFloat(this.id.split("_")[scoring_idx]);
+	var total = document.getElementById("total"+team);
+
+	if (document.getElementsByClassName("clicked").length === 0) {
+		resetTotal();
+	}
+
+	if ( (" " + this.className + " ").indexOf(" clicked ") > -1 )  {
+		// not clicked
+		this.className = this.className.split(" ")[0];
+		total.innerText = parseFloat(total.innerText) - val;
+	} else {		
+		this.className += " clicked";
+		total.innerText = parseFloat(total.innerText) + val;
+	}
+	findBest();
+}
+
+function last_updated(date) {
+	const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var week = date.split(",")[0];
+	var date = new Date(date.split(",")[1]);
+	document.getElementById("updated").getElementsByTagName("span")[0].innerText = "Last Updated: Week {} ({} {})".format(week, monthNames[date.getMonth()], date.getDate() + 1);	
+}
+
+function onGot(storage) {
+	players_picked = storage["players_picked"];
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			var j = JSON.parse(this.responseText);
+			last_updated(j["updated"]);
+			fillTable({...j, ...storage});
+			var tds = document.getElementsByClassName("player_td");
+			for (var i = 0; i < tds.length; ++i) {
+				if (tds[i].innerText != " - ") {
+					tds[i].addEventListener("click", increment, false);
+				}
+			}
+		}
+	};
+	xhttp.open("GET", "https://zhecht.pythonanywhere.com/extension"+storage["args"]);
+	xhttp.send();
+}
+
+function onError(error) {
+	console.log("ERROR: "+error);
+}
+
+function onExecScript() {
+	browser.storage.local.get().then(onGot, onError);
+}
+
+function onExecScriptErr(error) {
+	console.log("ERROR: "+ error);
+}
+
+var players_picked = false;
+browser.tabs.executeScript({file: "/content_scripts/main.js"}).then(onExecScript).catch(onExecScriptErr);
+
+var btns = document.getElementsByTagName("button");
+for (var i = 0; i < btns.length; ++i) {
+	btns[i].addEventListener("click", changeScoring, false);
+}
