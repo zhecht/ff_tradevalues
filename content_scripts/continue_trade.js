8f149d6d0fad76a97c126c8773fb84225d45e1cc
Zachary Hecht <zhecht@eng-zhecht-mb.arb.dhcp.cuda-inc.com>
2019-09-16 12:42:32 -0400

    rest of sites response and continue_trade

--- content_scripts/continue_trade.js
+++ content_scripts/continue_trade.js
@@ -6,7 +6,32 @@ function onG(storage) {
 		for (var i = 0; i < links.length; ++i) {
 			if (links[i].innerText.trim() == "Continue") {
 				links[i].click();
+				return;
 			}
+		}
+	} else if (storage["is_espn"]) {
+		var btns = document.getElementsByClassName("action-buttons");
+		for (var i = 0; i < btns.length; ++i) {
+			if (btns[i].innerText.trim() == "Continue" || btns[i].innerText.trim() == "Finalize Trade Offer") {
+				btns[i].click();
+				return;
+			}
+		}
+	} else if (storage["is_nfl"]) {
+		document.querySelector("input.submit").click();
+	} else if (storage["is_cbs"]) {
+		document.getElementById("reviewCurrentTrade").click();
+	} else if (storage["is_sleeper"]) {
+		// now check if receive window is up
+		var win = document.getElementsByClassName("set-trade-player-modal");
+		if (win.length > 0) {
+			var btn = document.getElementsByClassName("set-trade-player-modal")[0].getElementsByTagName("button")[0];
+			if (btn.innerText.split(" ")[0].toLowerCase() == "receive") {				
+				btn.click();
+			}
+		} else {
+			// overview 
+			document.getElementsByClassName("propose-btn")[0].click();
 		}
 	}
 }
--- content_scripts/response.js
+++ content_scripts/response.js
@@ -3,6 +3,7 @@
 var double_team = ["los angeles", "san francisco", "new york", "green bay", "tampa bay", "new england", "new orleans", "kansas city"];
 function onG(storage) {
 	var name = storage["clicked_name"].toLowerCase();
+
 	if (storage["is_yahoo"]) {
 		var name_parts = name.split(" ");
 		if (double_team.indexOf(name) == -1 && name_parts.length > 1) {
@@ -11,9 +12,35 @@ function onG(storage) {
 				name += (" "+name_parts[i]);
 			}
 		}
+		var id = storage["input_ids"][name];
+		if (id)
+			document.getElementById(id).checked = storage["should_click"];
+	} else if (storage["is_espn"]) {
+		// keys are 'table_idx', 'btn_idx'
+		var step2 = window.location.search.substring(1).indexOf("step=2") !== -1;
+		var data = storage["input_ids"][name];
+		if (step2) {
+			var tables = document.getElementsByClassName("propose-trade-content")[0].children[1].children;
+		} else {
+			var tables = document.getElementsByClassName("players-table");
+		}
+		var btn = tables[data.table_idx].getElementsByTagName("button")[data.btn_idx];
+		btn.click();
+	} else if (storage["is_nfl"]) {
+		var id = storage["input_ids"][name];
+		if (id)
+			document.getElementById(id).click();
+	} else if (storage["is_cbs"]) {
+		var id = storage["input_ids"][name];
+		if (id)
+			document.querySelector("input[name='"+id+"'").click();
+	} else if (storage["is_sleeper"]) {
+		var win = document.getElementsByClassName("set-trade-player-modal");
+		var rows = win[0].getElementsByClassName("team-roster-item");
+		var id = storage["input_ids"][name];
+		if (id || id == 0)
+			rows[id].click();
 	}
-	var id = storage["input_ids"][name];
-	document.getElementById(id).checked = storage["should_click"];
 }
 
 function onE(e) {
